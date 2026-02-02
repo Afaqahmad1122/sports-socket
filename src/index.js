3d90a524cd82b95a2c2b1f80d07418d0effeb697
Afaq Ahmad <afaqahmadnaeem@gmail.com>
2026-02-02 19:04:37 +0500

    implement matches post and get request

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import express from "express";
+import matchesRoutes from "./routes/matches.js";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -7,6 +8,9 @@ app.use(express.json());
 app.get("/", (req, res) => {
   res.send("Hello World");
 });
+
+// routes
+app.use("/matches", matchesRoutes);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
